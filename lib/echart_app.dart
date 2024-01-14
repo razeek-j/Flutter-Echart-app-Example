3e5b3590fbaa2ad004b65ac6db79119f57a21168
Mohammed Moosa Razeek J <moosarazeek@gmail.com>
2024-01-14 23:04:05 +0530

    Heading

---
+++ lib/echart_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Echartapp extends StatelessWidget{
+  const Echartapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Echart Representation')),
+
+      )
+    );
+  }}
